9cd288e090a3e6efc857e0e8035d1ac5cff2a396
nkvelkov <nkvelkov>
2018-03-11 13:55:12 +0200

    Added comments and removed an unnecessary include statement.

--- exercise2/main.cpp
+++ exercise2/main.cpp
@@ -9,13 +9,13 @@ using namespace std;
 
 int main()
 {
-    Point zeroZeroPoint;
+    Point zeroZeroPoint;   // here Point() is called, so x and y are 0.
     zeroZeroPoint.Print();
 
-    Point fiveZeroPoint(5);
+    Point fiveZeroPoint(5); // here Point(int y) is called so x is 5, y is 0.
     fiveZeroPoint.Print();
 
-    Point customPoint(2, 3);
+    Point customPoint(2, 3); // here Point(int x, int y) is called so x is 2, y is 3
     customPoint.Print();
 
     Robot robot; // a call to Robot::Robot() here
@@ -31,7 +31,7 @@ int main()
     testRobot.setProductivity(newProductivity);
 
     char newRobotName[ROBO_NAME_MAX_LEN];
-    strcpy(newRobotName, "newTestName");
+    strcpy(newRobotName, "newTestName"); // or strcpy_s(newRobotName, "newTestName"); if you are using VS.
 
     testRobot.setName(newRobotName);
 
@@ -42,3 +42,4 @@ int main()
 
     return 0;
 }
+
